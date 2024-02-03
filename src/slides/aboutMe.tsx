77e895104854466dbc89d4291bfd4e8bd40d65bb
Asela Pasindu <asela.pasindu.dias17@gmail.com>
2024-02-03 23:30:15 +0530

    home page style and about me page style update to 1024px screen size

--- src/slides/aboutMe.tsx
+++ src/slides/aboutMe.tsx
@@ -26,7 +26,7 @@ const ChildrenWithProps = (childrenProps: childrenProps) => {
 
     return (
         <div className={`aboutme flex flex-col bg-black w-[100%] h-[100%] ${className? className : ""}`}>
-            <div className=" flex flex-col text-[1.8rem] absolute right-[200px] top-[200px] ">
+            <div className=" flex flex-col text-[1.8rem] absolute right-[110px] top-[140px] ">
                 <button>
                     <h1 className=" text-start leading-tight ">SKILLS</h1>
                 </button>
@@ -38,26 +38,26 @@ const ChildrenWithProps = (childrenProps: childrenProps) => {
                 </button>
             </div>
             <SectionNavBar title="WHO AM I" handleBackButton={handleClick} />
-            <div className=" flex-grow flex flex-col ml-[120px] justify-evenly text-[1.5rem] leading-none ">
-                <p className=" w-[500px] font-bold ">
+            <div className=" flex-grow flex flex-col ml-[90px] justify-evenly text-[1.1rem] leading-tight ">
+                <p className=" w-[400px] font-bold ">
                     Hello. I'm Asela Pasindu Dias. I'm a multi-talented person who has over 4+ years of experience in a variety of design disciplines, coding, and research fields.
                 </p>
-                <p className=" w-[500px] ml-[130px] font-light ">
+                <p className=" w-[400px] ml-[100px] font-light ">
                     Additionally, I'm Doing UI/UX design, software development, and full-stack web development, Three.JS development, C# development. I'm also interested in varieties of programming languages and the concept of disciplines in programming.
                 </p>
-                <p className=" w-[624px] ml-[280px] font-light ">
+                <p className=" w-[480px] ml-[220px] font-light ">
                     I prefer not to use my past work to define who I am. I define myself by the work I want to do. Although personality cannot be taught, skills can. I prefer to continue learning, to challenge myself, and to engage in worthwhile activities.
                 </p>
-                <p className=" ml-[280px] font-light ">
+                <p className=" ml-[220px] font-light ">
                     I'm passionate about learning new things and conducting research. I have published on Medium.com about my research and my ideas. I'm an enthusiast of science, quantum physics, astronomy, and UAP (UFO).
                 </p>
-                <p className=" w-[688px] font-light ">
+                <p className=" w-[500px] font-light ">
                     Over the course of my life and profession, I like to gain competence in a variety of subjects. I am an ENFJ-T personality type. I now conduct remote work with a chosen group of freelancing clients, but I'm always interested in new opportunities.
                 </p>
             </div>
-            <div className=" absolute top-[268px] ml-[53px] ">
-                <svg height="500">
-                    <polygon points="0,0 0,360 250,500" fill="transparent" stroke="#ff6600" stroke-width="3" />
+            <div className=" absolute top-[222px] ml-[34px] ">
+                <svg height="385">
+                    <polygon points="2,6 2,260 220,380" fill="transparent" stroke="#ff6600" stroke-width="3" />
                 </svg>
             </div>
             <ContactButtons />
