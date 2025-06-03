ffa641f6d16b9d4a626985b9106dad5f4bcb47d4
aselapasinduD <asela.pasindu.dias17@gmail.com>
2025-06-03 15:10:16 +0530

    Stage: Development | Area: Experince Slide | Work: Added new education & fixed mobile not responsive

--- src/slides/sections/experiences.tsx
+++ src/slides/sections/experiences.tsx
@@ -18,6 +18,14 @@ type childrenProps = props & ismobile;
 
 const educations = [
     {
+        title: "University Of Moratuwa - Bachelor of Information Technology",
+        courseList: [
+            {
+                name: "Undergraduate"
+            }
+        ]
+    },
+    {
         title: "UOM Open Learning - Full Stack Development",
         courseList: [
             {
@@ -87,8 +95,8 @@ const ChildrenWithProps = (childrenProps: childrenProps) => {
     return(
         <div className="bg-black w-[100%] h-[100%] overflow-hidden">
             <SectionNavBar title={titleAnimate[0]} handleBackButton={() => dispatch(closeSection('experiencesSection'))} ismobile={ismobile} />
-            <div className="bg-[#d9d9d920] w-full h-full">
-                <div className="programmingLanguageList relative w-9/12 flex h-[--height] m-auto overflow-hidden" style={programmingLanguageListStyle}>
+            <div className="bg-[#d9d9d920] w-full h-full flex flex-col">
+                <div className="programmingLanguageList relative w-9/12 flex h-[--height] mx-auto my-4 overflow-hidden" style={programmingLanguageListStyle}>
                     <div className="absolute flex gap-5 h-[--height] left-[100%]" style={programmingLanguageList1}>
                         {programmingLanguageList.map((programmingLanguage, index) => {
                             return <h1 key={index} id={`programmingLanguageListItem-${index}`} className="text-[3rem] text-center text-[var(--secondary-color)]">{programmingLanguage}</h1>
@@ -100,11 +108,11 @@ const ChildrenWithProps = (childrenProps: childrenProps) => {
                         })}
                     </div>
                 </div>
-                <div className="w-9/12 m-auto">
+                <div className="w-11/12 md:w-9/12 m-auto overflow-y-scroll flex-1">
                     {educations.map((education, index) => 
                         <div key={index}>
                             <h4 className="text-center text-600 text-[1.8rem]">{education.title}</h4>
-                            <div className="flex gap-5 justify-center">
+                            <div className="flex flex-wrap gap-5 justify-center">
                                 {education.courseList.map((course, index) =>
                                     <p key={index} className="w-[200px] text-center text-[1.15rem]">{course.name}</p>
                                 )}
